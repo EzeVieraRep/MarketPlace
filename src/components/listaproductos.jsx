6263bbac8b2d7e4966505af7060983ae0a5f0eb6
EzeVieraRep <eviera32@gmail.com>
2023-04-12 19:55:43 -0300

    componentes actualizados

--- src/components/listaproductos.jsx
+++ src/components/listaproductos.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import TarjetaProducto from './product-card'
 
+const Tarjeta1 = {
+  id: 1,
+  nombre: 'Tarjeta 1',
+  precio: 1000,
+  cantidad: 1,
+  imagen: 'https://www.tarjetas.com/wp-content/uploads/2019/05/tarjeta-1.jpg'
+}
+const Tarjeta2 = {
+  id: 2,
+  nombre: 'Tarjeta 2',
+  precio: 2000,
+  cantidad: 2,
+  imagen: 'https://www.tarjetas.com/wp-content/uploads/2019/05/tarjeta-2.jpg'
+}
+const Tarjeta3 = {
+  id: 3,
+  nombre: 'Tarjeta 3',
+  precio: 3000,
+  cantidad: 3,
+  imagen: 'https://www.tarjetas.com/wp-content/uploads/2019/05/tarjeta-3.jpg'
+}
+const arrayProductos = [Tarjeta1,Tarjeta2,Tarjeta3]
 
 const Listaproductos = () => {
 
   return (
     <div>
-        <TarjetaProducto />
+    {arrayProductos.map((producto)=>{
+      return <TarjetaProducto key={producto.id} nombre={producto.nombre} precio={producto.precio} cantidad={producto.cantidad} imagen={producto.imagen} />
+    })}
     </div>
   )
 }
--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const TarjetaProducto = () => {
+const TarjetaProducto = ({nombre, precio, cantidad, imagen}) => {
+
+
   return (
+
     <div>
     <h2> Tarjeta Producto</h2>
+    <img src={imagen} alt={nombre} />
+    <p>Nombre: {nombre}</p>
+    <p>Precio: {precio}</p>
+    <p>Cantidad: {cantidad}</p>
     </div>
   )
 }
