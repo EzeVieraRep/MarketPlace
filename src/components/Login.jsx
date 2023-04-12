d296cafe19e1703e0b4d04341d2a5404a5283765
kirinico <kirinico@gmail.com>
2023-04-12 20:42:27 -0300

    se instalo boostrap

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+    return (
+        <div className="container">
+            <form className="form">
+                <p>login</p>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Listaproductos from "../components/listaproductos.jsx"
+import Login from "../components/Login.jsx";
 
 const AppRouter = () => {
-    return ( 
+    return (
         <Routes>
             <Route exact path="/" element={<Listaproductos />} />
+            <Route exact path="/login" element={<Login />} />
+
         </Routes>
-     );
+    );
 }
- 
+
 export default AppRouter;
