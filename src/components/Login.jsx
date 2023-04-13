7b6cf77f6aea963f89a7d7306dbb1ca9cdd33a61
kirinico <kirinico@gmail.com>
2023-04-13 01:38:16 -0300

    se instalo boostrap icon y se implemento en login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,7 +37,10 @@ const Login = () => {
         <div className="container">
             <form className="form" onSubmit={onSubmit}>
                 <div className="avatar_Container">
-                    <i className="bi bi-person-circle"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                    </svg>
                 </div>
                 <div>
                     <h3 className="title">Iniciar Sesion</h3>
@@ -45,7 +48,7 @@ const Login = () => {
                 <div className="textField_container">
                     <Form.Group className="mb-3">
                         <Form.Label>email</Form.Label>
-                        <Form.Control type='email' placeholder="ingrese el email" name="email" onChange={changeInput}/>
+                        <Form.Control type='email' placeholder="ingrese el email" name="email" onChange={changeInput} />
                     </Form.Group>
                 </div>
                 <div className="textField_container">
@@ -55,7 +58,7 @@ const Login = () => {
                     </Form.Group>
                 </div>
                 <div className="button_container">
-                    <Button variant="primary" type="submit" disabled={loginButtonDisabled()}>INICIAR SESION</Button>
+                    <Button variant={!loginButtonDisabled() ? "primary" : "secondary"} type="submit" disabled={loginButtonDisabled()}>INICIAR SESION</Button>
                 </div>
                 <div className="link_container">
                     <Link to="#" className="link">
