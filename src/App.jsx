0e0638d92b8200baac13f030f061e50a5a93783a
EzeVieraRep <eviera32@gmail.com>
2023-04-13 23:03:13 -0300

    Cambios Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
       <Router>
+      <Header />
         <AppRouter />
       </Router>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "./Header.css"
+
+function Header() {
+  return (
+    <Navbar bg="primary" variant="dark" className='navbarNew'>
+      <Container>
+      <img src='../img/5087847.png' alt='carrito'></img>
+          <Navbar.Brand href="#">MarketPlace</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#">Inicio</Nav.Link>
+            <Nav.Link href="#">Productos</Nav.Link>
+          </Nav>
+          <Nav>
+          <Nav.Link href="#">Iniciar Sesion</Nav.Link>
+          <Nav.Link href="#">Registrarse</Nav.Link>
+          </Nav>
+      </Container>
+      </Navbar>
+  );
+}
+
+export default Header;
