55b30d6a2d5af7b3d65371ae88d10489ab1cefbf
kirinico <kirinico@gmail.com>
2023-04-14 21:28:29 -0300

    ahora se puede logearse

--- src/pages/login/components/UseLogin.jsx
+++ src/pages/login/components/UseLogin.jsx
@@ -1,6 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Login from '../views/Login'
+import { useNavigate } from 'react-router-dom';
+
+
+
+const USER = {
+    username: "admin@admin.com",
+    password: "admin"
+}
 const useLogin = () => {
+    const navigate = useNavigate();
 
     // hook de user
     const [user, setUser] = useState({
@@ -8,6 +17,7 @@ const useLogin = () => {
         password: "",
     });
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
     // destroyoning del hook user
     const { email, password } = user;
 
@@ -25,7 +35,33 @@ const useLogin = () => {
     const onSubmit = (e) => {
         e.preventDefault();
 
+        login( email, password)
     };
+
+    // esto tiene que ir en un context
+    const login = (email, password) => {
+        if (USER.username === email) {
+            if (USER.password === password) {
+                setIsLoggedIn(true);
+                localStorage.setItem('isLoggedIn', true);
+                navigate("/")
+            } else {
+                console.log("la contraseña esta mal")
+                localStorage.setItem('isLoggedIn', false);
+            }
+
+        } else {
+            console.log("el email esta mal")
+            localStorage.setItem('isLoggedIn', false);
+        }
+    }
+
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            navigate("/")
+        }
+    }, [])
 
     return (
         <Login loginButtonDisabled={loginButtonDisabled} changeInput={changeInput} onSubmit={onSubmit} />
