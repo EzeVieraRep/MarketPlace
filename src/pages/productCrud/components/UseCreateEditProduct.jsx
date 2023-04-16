1aa57246857e3081da76d04a0ca0639072876677
kirinico <kirinico@gmail.com>
2023-04-16 19:23:34 -0300

    se hizo el componente createEditProduct y se le pusieron estilos

--- src/pages/productCrud/components/UseCreateEditProduct.jsx
+++ src/pages/productCrud/components/UseCreateEditProduct.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import CreateEditProduct from "../views/CreateEditProduct";
+
 const UseCreateEditProduct = () => {
-    return ( 
-        <div>hola</div>
-     );
+
+    //hook de imagen seleccionada 
+    const [selectedImage, setSelectedImage] = useState("")
+    const [images, setImages] = useState([])
+
+    //selecciona una imagen del producto haciendo click
+    const selectImage = (image) => setSelectedImage(image)
+
+    const imageChange = (e) => setImages(images.concat(e.target.files[0]));
+
+    return <CreateEditProduct images={images} selectImage={selectImage} selectedImage={selectedImage} imageChange={imageChange}/>
 }
- 
+
 export default UseCreateEditProduct;
--- src/pages/productCrud/views/CreateEditProduct.jsx
+++ src/pages/productCrud/views/CreateEditProduct.jsx
@@ -1,7 +1,85 @@
-const CreateEditProduct = () => {
-    return ( <div>
-        create Producto
-    </div> );
+import { Button, Form } from "react-bootstrap";
+import '../Styles.css'
+
+const CreateEditProduct = ({ images, selectImage, selectedImage, imageChange }) => {
+    return (
+        <div className="product_container">
+            <div className="product_card">
+                <h1>Agregar Producto</h1>
+                <form className="product_form">
+                    {/* NOMBRE */}
+                    <div className="product_item">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Nombre</Form.Label>
+                            <Form.Control type="text" placeholder="ingrese el nombre del producto" name="name" />
+                        </Form.Group>
+                    </div>
+                    {/* PRECIO */}
+                    <div className="product_item">
+                        <Form.Group className="mb-3">
+                            <Form.Label>precio</Form.Label>
+                            <Form.Control type="number" placeholder="ingrese el precio  del producto" name="price" />
+                        </Form.Group>
+                    </div>
+                    {/* DESCRIPCION */}
+                    <div className="product_item">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Descripcion</Form.Label>
+                            <Form.Control type="text" placeholder="ingrese la descripcion del producto" name="descripcion" />
+                        </Form.Group>
+                    </div>
+                    {/* STOCK */}
+                    <div className="product_item">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Stock</Form.Label>
+                            <Form.Control type="number" placeholder="ingrese el stck del producto" name="stock" />
+                        </Form.Group>
+                    </div>
+                    {/* CATEGORIA */}
+                    <div className="product_item">
+                        <Form.Group className="mb-3">
+                            <Form.Select aria-label="Default select example">
+                                <option>Categoria</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </div>
+                    {/* SUBIR IMAGBEN */}
+                    <div className="product_item">
+                        <Form.Group controlId="formFile" className="mb-3" onChange={imageChange}>
+                            <Form.Control type="file" />
+                        </Form.Group>
+                        
+                    </div>
+                    {/* IMAGEN PREVIA */}
+                    <div className="product_item_col2">
+                        {
+                            images.length ?
+                                <div className="product_upload_img">
+
+                                    {images.map((image, i) =>
+                                        <div key={i} className={(selectedImage === image) ? "product_select_image" : null}>
+                                            <div variant="light" onClick={() => selectImage(image)}>
+                                                <img src={URL.createObjectURL(image)} alt="uploaded_image" className="addEdditProduct_img" />
+                                            </div>
+                                        </div>
+                                    )
+                                    }
+                                </div>
+                                : <h3>No hay Imagenes seleccionadas</h3>
+                        }
+
+                    </div>
+                    {/* ELIMINAR IMAGEN */}
+                    <div className="product_item">
+                        <Button variant="primary">Eliminar</Button>
+                    </div>
+                </form>
+            </div >
+        </div >
+    );
 }
- 
+
 export default CreateEditProduct;
