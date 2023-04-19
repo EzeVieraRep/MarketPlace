6cf27cc7a97e6b09b4ea9c8253d7491b3aeae440
kirinico <kirinico@gmail.com>
2023-04-18 22:23:20 -0300

    se agrego login al servidor

--- service/controller/authController.js
+++ service/controller/authController.js
@@ -4,10 +4,10 @@ import User from '../models/User'
 
 export const register = async (req, res) => {
     try {
-        const { password} = req.body
+        const { password } = req.body
 
         let user = req.body
-        
+
         const salt = await bcryptjs.genSalt(10)
         user.password = await bcryptjs.hash(password, salt)
 
@@ -20,6 +20,35 @@ export const register = async (req, res) => {
 
                 //mensaje de confirmacion
                 res.status(200).json({ token, msg: "se a registrado con exito" });
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "hubo un error" });
+
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.find({email})
+
+        console.log(user)
+        if (!user) return res.status(400).json({ msg: "el usuario no existe" });
+        
+        const correctPass = await bcryptjs.compare(password, user[0].password);
+        if (!correctPass) return res.status(401).json({ msg: "password incorrecto" });
+        
+        const payload = { user: { id: user.id } };
+        //firmar el JWT
+        jwt.sign(
+            payload, process.env.SECRETA, { expiresIn: 36000, },
+            (error, token) => {
+                if (error) throw error;
+
+                //mensaje de confirmacion
+                res.json({ token, msg: "se a logeado correctamente" });
             }
         );
     } catch (error) {
--- service/routes/auth.js
+++ service/routes/auth.js
@@ -1,10 +1,9 @@
 import { Router } from "express"
-import { register } from "../controller/authController"
+import { register, login } from "../controller/authController"
 const router = Router();
 
-
-
 router.post('/register', register)
+router.post("/login", login);
 
 
 module.exports = router;
